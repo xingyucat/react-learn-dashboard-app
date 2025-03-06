4031406552819b234e8fcf7abe3717cfed62fdde
星宇 <zrl-mail@qq.com>
2025-03-06 13:33:24 +0800

    feat(dashboard): 实现发票创建功能并优化发票页面
    - 新增 createInvoice 函数用于创建发票
    - 创建发票页面和表单组件
    - 优化发票列表页面，添加搜索和分页功能- 修复发票表格中长名字显示问题
    - 优化搜索组件，添加防抖功能

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -33,7 +33,7 @@ export default async function InvoicesTable({
                         height={28}
                         alt={`${invoice.name}'s profile picture`}
                       />
-                      <p>{invoice.name}</p>
+                      <p className="whitespace-break-spaces">{invoice.name}</p>
                     </div>
                     <p className="text-sm text-gray-500">{invoice.email}</p>
                   </div>
@@ -92,7 +92,7 @@ export default async function InvoicesTable({
                         height={28}
                         alt={`${invoice.name}'s profile picture`}
                       />
-                      <p>{invoice.name}</p>
+                      <p className="whitespace-break-spaces">{invoice.name}</p>
                     </div>
                   </td>
                   <td className="whitespace-nowrap px-3 py-3">
